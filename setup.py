cf0c315f7ce0c4c0bc87d16e94debfc86bed522b
henry <1151071+HenrYxZ@users.noreply.github.com>
2024-06-14 11:57:57 -0400

    chore: update setup.py package version to 1.0.0 to match the release version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="pyglet_particles",
     author="sombra-studio",
-    version="0.1.0",
+    version="1.0.0",
     description="particle systems for pyglet",
     license="MIT",
     install_requires=[
